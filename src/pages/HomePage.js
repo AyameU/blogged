8a904572662bdae770a577032024d0a639d9f454
AyameU <54376512+AyameU@users.noreply.github.com>
2021-05-26 13:00:01 -0500

    Added React Router, created homepage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+        <h1>Hello there... welcome to my blog!</h1>
+        <p>
+            I will add more text here.
+        </p>
+    </>
+);
+
+export default HomePage;
